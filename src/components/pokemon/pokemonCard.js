e7cf4140b91ca89838eaa737cee692ddd421fc5d
Eddie R <eddieranieri.prg@gmail.com>
2020-05-21 19:07:46 -0300

    Consumir api, manipular dados na view

--- src/components/pokemon/pokemonCard.js
+++ src/components/pokemon/pokemonCard.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react'
 
 export default class pokemonCard extends Component {
-    render() {
-        return (
+
+    state = {
+        name: '',
+        imageUrl: '',
+        pokemonIndex: '',
+    };
+
+
+
+
+   componentDidMount() {
+
+        const {name, url} = this.props;
+        const pokemonIndex = url.split('/')[url.split('/').length - 2 ];
+        const imageUrl = `https://github.com/PokeAPI/sprits/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+
+        this.setState({
+            name: name,
+            imageUrl: imageUrl,
+            pokemonIndex: pokemonIndex
+        });
+    }
+
+            render() {
+                return (
             <div className="col-md-3 col-sm-6 mb-5">
                 <div className="card">
-                    <div className="card-header">
-                        <h1>Eddie</h1>
-                    </div>
+                   
+                        <h5 className="card-header">{this.state.pokemonIndex}</h5>
+                        <div className="card-body mx-auto">
+                            <h6 className="card-title">
+                                {this.state.name
+                                .toLowerCase()
+                                .split(" ")
+                                .map(
+                                    letter => letter.charAt(0).toUpperCase() + letter.substring(1)
+                                 )
+                                  .join(' ')
+                             }
+                                    </h6>
+                        </div>
+                        
+                    
                 </div>
                 
             </div>
-        )
+        );
     }
 }
